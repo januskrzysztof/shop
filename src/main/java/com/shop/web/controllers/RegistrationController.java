16d9077f26641cd30b08b4ae421b0f269a408b27
Krzysztof Januś <janus.krzysztof@onet.pl>
2015-02-06 12:18:31 +0100

    Fix pom.xml and add apache tiles 3

--- src/main/java/com/shop/web/controllers/RegistrationController.java
+++ src/main/java/com/shop/web/controllers/RegistrationController.java
@@ -3,7 +3,6 @@ package com.shop.web.controllers;
 import com.shop.exceptions.DaoException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Krzysztof Januś on 2015-02-05.
@@ -11,8 +10,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class RegistrationController {
     @RequestMapping("/registration")
-    public ModelAndView registration(ModelAndView model) throws DaoException {
-        model.setViewName("registration");
-        return model;
+    public String registration() throws DaoException {
+        return "registration";
     }
 }
--- src/main/webapp/WEB-INF/views/jsp/layouts/layout.jsp
+++ src/main/webapp/WEB-INF/views/jsp/layouts/layout.jsp
@@ -1,14 +1,7 @@
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-<head>
-    <meta charset="utf-8">
+<%@ taglib uri="http://tiles.apache.org/tags-tiles" prefix="tiles" %>
 
-    <title></title>
-    <link rel="stylesheet" type="text/css" href="/css/styles.css" />
-</head>
-<body>
-    <h1>Layout.jsp</h1>
-    <div id="container"><jsp:include page="${page}" /></div>
-</body>
-</html>
+<h1>Layout.jsp</h1>
+
+<div>
+    <tiles:insertAttribute name="body" />
+</div>
